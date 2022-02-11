e6335c3a838871e7a7af74c399fa09312aeee5b2
Youssef Agagg <youssefagagg@gmail.com>
2022-02-11 14:12:24 +0200

    new build

--- src/app/view-all-customers/view-all-customers.component.ts
+++ src/app/view-all-customers/view-all-customers.component.ts
@@ -6,20 +6,30 @@ import { CustomerService } from '../customer.service';
 
 @Component({
   selector: 'app-view-all-customers',
-  templateUrl: './view-all-customers.component.html'
+  templateUrl: './view-all-customers.component.html',
+  styleUrls: ['./styles.css']
 })
 export class ViewAllCustomersComponent implements OnInit {
   public customers: Customer[] = [];
+  public showProgressBar=true;
+  
 
   constructor( private customerService:CustomerService,private router: Router) { }
 
   ngOnInit(): void {
+   
     this.getCustomers()
+    
+      
+
   }
   public getCustomers():void{
+
     this.customerService.getCustomers().subscribe(
       {
+       
         next: (response:Customer[])=>{
+         this.showProgressBar=false;
         this.customers=response;
         console.log(response);
       },
